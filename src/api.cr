b217c4af170822431bf28482eab4a5590370a4be
Ned Palacios <npdoesmc@gmail.com>
2019-05-24 15:48:35 +0800

    simplifying api setup process

---
+++ src/api.cr
@@ -0,0 +1,27 @@
+module Mdex
+  module API
+    def manga(id : Int32)
+      Mdex::Endpoints::Manga.get(id)
+    end
+
+    def group(id : Int32)
+      Mdex::Endpoints::Group.get(id)
+    end
+
+    def genre(id : Int32)
+      Mdex::Endpoints::Genre.get(id)
+    end
+
+    def search(query : String, options : Hash(String, String) = {} of String => String, fields_only : Bool = false)
+      Mdex::Endpoints::Genre.get(query, options, fields_only)
+    end
+
+    def updates
+      Mdex::Endpoints::Updates.get
+    end
+
+    def user(id : Int32)
+      Mdex::Endpoints::User.get(id)
+    end
+  end
+end
--- src/client.cr
+++ src/client.cr
@@ -2,10 +2,20 @@ require "http/client"
 
 module Mdex
   class Client
+    include Mdex::API
+
+    @@base_url = "https://mangadex.org/"
+
+    def initialize; end;
+
     def self.get(endpoint : String, params = {} of String => String)
       _params = HTTP::Params.encode(params)
 
-      HTTP::Client.get("https://mangadex.org/#{endpoint}?#{params}")
+      HTTP::Client.get("#{@@base_url}#{endpoint}?#{params}")
+    end
+
+    def self.base_url
+      @@base_url
     end
   end
 end
