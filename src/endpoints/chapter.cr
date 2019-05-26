7e930389bf9609cb6bcf3927551ed04b48894d88
Ned Palacios <npdoesmc@gmail.com>
2019-05-26 19:02:10 +0800

    fixed issues

--- src/endpoints/chapter.cr
+++ src/endpoints/chapter.cr
@@ -5,11 +5,15 @@ module Mdex::Endpoints
     alias ChapterImagesInfo = String | Int32 | JSON::Any | Array(JSON::Any)
     alias ChapterImages = Hash(String, ChapterImagesInfo)
 
+    @@images_hash = {} of String => ChapterImagesInfo
+
     def self.get(id : Int32)
       response = Mdex::Client.get("api/chapter/#{id}")
 
       case response.status_code
       when 200
+        @@images_hash["id"] = id
+
         data = JSON.parse(response.body)
         parse_data(data)
       when 404
@@ -21,16 +25,13 @@ module Mdex::Endpoints
     end
 
     private def self.parse_data(data : JSON::Any)
-      images_hash = {} of String => ChapterImagesInfo
+      @@images_hash["manga_id"] = data["manga_id"]
+      @@images_hash["page_length"] = data["page_array"].as_a.size
+      @@images_hash["server_url"] = data["server"].as_s === "/data/" ? "https://s4.mangadex.org/data/" : data["server"].as_s
+      @@images_hash["pages"] = data["page_array"].as_a
+      @@images_hash["long_strip"] = data["long_strip"].as_i
 
-      images_hash["id"] = id
-      images_hash["manga_id"] = data["manga_id"]
-      images_hash["page_length"] = data["page_array"].as_a.size
-      images_hash["server_url"] = data["server"].as_s === "/data/" ? "https://s4.mangadex.org/data/" : data["server"].as_s
-      images_hash["pages"] = data["page_array"].as_a
-      images_hash["long_strip"] = data["long_strip"].as_i
-
-      images_hash.as(ChapterImages).to_json
+      @@images_hash.as(ChapterImages).to_json
     end
   end
 end
