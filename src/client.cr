7880a08a47bcd8ac761a3784f9be4a93d47a3c81
Ned Palacios <npdoesmc@gmail.com>
2019-05-27 14:29:32 +0800

    replace default http with halite

--- src/client.cr
+++ src/client.cr
@@ -1,17 +1,18 @@
-require "http/client"
+require "halite"
 
 module Mdex
   class Client
     include Mdex::API
 
     @@base_url = "https://mangadex.org/"
+    @@client = Halite::Client.new
 
     def initialize; end;
 
     def self.get(endpoint : String, params = {} of String => String)
       _params = HTTP::Params.encode(params)
 
-      HTTP::Client.get("#{@@base_url}#{endpoint}?#{params}")
+      @@client.get("#{@@base_url}#{endpoint}", params: params)
     end
 
     def self.base_url
